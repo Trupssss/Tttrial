c8c80e53e12616557d531cd9b62e1c89d7641c44
Trupssss <tru.ptiissc@gmail.com>
2017-03-02 11:11:54 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,11 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<link rel="stylesheet" href="style.css" />
 </head>
-<body>
+body
+{
+  background-image: url("images/Tour8.jpg");
+  background-color: #FF6347;
+}
 	<center>
 		<table>
 			<tr>
@@ -18,16 +22,29 @@
 				<p class='description'></p> We will help you <span class="blue">make your tour beautiful</span>!<br>
 			     </td>
 			</tr>
+			<tr>
+			     <td>
 				<img src='images/Tour6.jpg'> <br>
-			
+			     </td>
+			</tr>
+			<tr>
+			     <td>
 				<h5 id = "message"><?php echo "Select location :"; ?></h5>	<br>
 				<select name="Location">
 					<option value="">SPPU</option>
 					<option value="M">Mumbai</option>
 					<option value="P">Paris</option>
 				</select>
+			     </td>
+			     <td>
 				<input id='Submit' name='Submit' value='Go' type='button'>	<br>
+			     </td>
+			 </tr>
+			 <tr>
+			     <td>
 				<img src='images/Tour7.jpg'>
+			     </td>
+			 </tr>
 		</table>
 	</center>
 </body>
