45677dca9b8e1aa4516ad771757addb7a40887c2
Trupssss <tru.ptiissc@gmail.com>
2017-03-02 16:09:35 +0530

    Update index.php

--- index.php
+++ index.php
@@ -10,22 +10,28 @@
 	   <table>
 	      <tr>
 		 <td>
-		<img class = 'newappIcon' src='images/Tour7.jpg'>
-		 </td>
-		 <td>
-		<h1 id = "message"><?php echo "Watson Tour Guide"; ?></h1>
+			<img class = 'newappIcon' src='images/Tour7.jpg'>
+			<h1 id = "message"><?php echo "Watson Tour Guide"; ?></h1>
 	         </td>
 	      </tr>
 	   </table>
 		<p class='description'></p> We will help you <span class="yellow">make your tour beautiful</span>!<br><br>
 			<img src='images/Tour6.jpg'> <br>
-			<h2 id = "message"><?php echo "Select location :"; ?></h2>	
+		<tr>
+		 <td>
+			<h2 id = "message"><?php echo "Select location :"; ?></h2>
+		 </td>
+		  <td>
 				<select name="Location" style="width: 150px;">
 					<option value="">SPPU</option>
 					<option value="M">Mumbai</option>
 					<option value="P">Paris</option>
 				</select>
+		  </td>
+		  <td>
 			<input id='Submit' name='Submit' value='Go' type='button' class='Go'>
+	          </td>
+	      </tr>
 	</center>
 </body>
 </html>
