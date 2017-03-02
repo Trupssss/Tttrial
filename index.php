55e07f695fe7877caef4924d4e0ec5a5a35b653a
Trupssss <tru.ptiissc@gmail.com>
2017-03-02 11:27:03 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,24 +7,17 @@
 </head>
 <body>
 	<center>
-
-				<h1 id = "message"><?php echo "Watson Tour Guide"; ?></h1>	<br>
-
-				<p class='description'></p> We will help you <span class="blue">make your tour beautiful</span>!<br>
-	
-				<img src='images/Tour6.jpg'> <br>
-		
-				<h5 id = "message"><?php echo "Select location :"; ?></h5>	<br>
+		<h1 id = "message"><?php echo "Watson Tour Guide"; ?></h1>	<br>
+		<p class='description'></p> We will help you <span class="blue">make your tour beautiful</span>!<br>
+			<img src='images/Tour6.jpg'> <br>
+			<h5 id = "message"><?php echo "Select location :"; ?></h5>	<br>
 				<select name="Location">
 					<option value="">SPPU</option>
 					<option value="M">Mumbai</option>
 					<option value="P">Paris</option>
 				</select>
-		
-				<input id='Submit' name='Submit' value='Go' type='button'>	<br>
-			   
-				<img src='images/Tour7.jpg'>
-			
+			<input id='Submit' name='Submit' value='Go' type='button'>	<br>
+			<img src='images/Tour7.jpg'>
 	</center>
 </body>
 </html>
